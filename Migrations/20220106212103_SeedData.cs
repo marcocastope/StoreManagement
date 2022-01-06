d3f0d00ec01ca083f2f19b84048c233621d0443d
Marco Castope <marcocastope7@gmail.com>
2022-01-06 18:42:38 -0500

    db migrations added

---
+++ Migrations/20220106212103_SeedData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoreManagement.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "OrderDate", "OrderNumber" },
+                values: new object[] { 1, new DateTime(2022, 1, 6, 21, 21, 2, 663, DateTimeKind.Utc).AddTicks(1905), "12345" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
