548c2c5fee1aa10af64231cfa9765f36cccf3d4f
Marco Castope <marcocastope7@gmail.com>
2022-01-06 18:38:40 -0500

    controllers added

---
+++ Controllers/AppController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreManagement.Data;
+using System;
+using System.Linq;
+
+namespace StoreManagement.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IStoreRepository repository;
+
+        public AppController(IStoreRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet("shop")]
+        public IActionResult Shop()
+        {
+            var result = repository.GetAllProducts();
+            return View(result);
+        }
+    }
+}
